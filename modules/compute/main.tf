1eefcc8d9f920ab6fadc5c156ce1c33f642ba64c
Kartik Choubey <155246897+Kar213@users.noreply.github.com>
2025-10-27 12:54:33 +0530

    Configure Terraform backend with S3 and DynamoDB

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "drift-demo-compute"
+    key            = "compute/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
 provider "aws" {
   region = var.region
 }
