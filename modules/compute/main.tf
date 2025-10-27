e4a341fdc090c10dc7d4d41e4cf2daf24742cedc
Kartik Choubey <155246897+Kar213@users.noreply.github.com>
2025-10-27 12:58:11 +0530

    Change S3 bucket name for Terraform backend

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "drift-demo-compute"
+    bucket         = "drift-demo"
     key            = "compute/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
