53bccf9a77d7684d366279214fd7411ff60e847d
Kartik Choubey <155246897+Kar213@users.noreply.github.com>
2025-10-27 11:21:55 +0530

    variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region where VPC will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+  default     = "demo-vpc"
+}
